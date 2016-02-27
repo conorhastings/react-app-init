bcd32675fbab4adad19333ce7d113ff0ea4872f2
Conor Hastings <hastings.conorm@gmail.com>
2016-02-27 11:51:52 -0500

    init commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import { Provider } from 'react-redux';
+import React from 'react';
+import { render } from 'react-dom';
+
+class Root extends React.Component {
+  componentDidMount() {
+    const { store, init } = this.props;
+    init.forEach(action => store.dispatch(action()));
+  }
+  render() {
+    const { store, children } = this.props;
+    return <Provider store={store}>{children}</Provider>
+  }
+}
+
+export default function(component, {store, init = [], renderTarget}) {
+  if (!renderTarget) {
+    document.body.innerHTML = '';
+    const div = document.createElement('div');
+    div.id = 'app';
+    document.body.appendChild(div);
+    renderTarget = document.getElementById('app');
+  }
+  render(<Root store={store} init={init}>{component}</Root>, renderTarget);
+}
+
